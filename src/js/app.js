70cd9dd89031885e346c6c566cd97f36631f4224
Evgeniy Chausov <evgeniy@chausov.ru>
2024-08-15 16:42:07 +0300

    Done H/W

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+const text = document.querySelector(".text");
+
+document.querySelector(".button").addEventListener("click", () => {
+  if (text.classList.contains("open")) {
+    text.classList.remove("open");
+  } else {
+    text.classList.add("open"); 
+  }
+});
